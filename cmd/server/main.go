8d28808407885dd3b7f66290475342cd40e47395
codevski <1435321+codevski@users.noreply.github.com>
2026-03-25 15:22:16 +1100

    feat: graceful shutdown on SIGTERM/SIGINT

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"stalkarr/internal/api"
 	"stalkarr/internal/config"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +21,7 @@ func main() {
 
 	dataDir := os.Getenv("DATA_DIR")
 	if dataDir == "" {
-		dataDir = "./config"
+		dataDir = "/config"
 	}
 
 	if err := config.Init(dataDir); err != nil {
@@ -31,6 +36,30 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Stalkarr running on :%s (data: %s)", port, dataDir)
-	r.Run(":" + port)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	go func() {
+		log.Printf("Stalkarr running on :%s (data: %s)", port, dataDir)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Println("Shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("forced shutdown: %v", err)
+	}
+
+	log.Println("Stalkarr stopped")
 }
